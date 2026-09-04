57b93a05eea2465048173e984d7f483f83f92d5b
406-30-teepakron <54653@yupparaj.ac.th>
2026-09-04 11:01:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if "ans2_val" not in st.session_state:
 if "ans3_val" not in st.session_state:
     st.session_state.ans3_val = ""
 if "ans4_val" not in st.session_state:
-    st.session_state.ans4_val = "
+    st.session_state.ans4_val = ""
 
 
   st.divider()
